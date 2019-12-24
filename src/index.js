bbb95b552af4f0a0558f5e6a8d7f1e032b17b875
Sreevishnu <sreeab28@gmail.com>
2019-12-25 00:32:37 +0530

    implementing table content

--- src/index.js
+++ src/index.js
@@ -1,38 +1,55 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
+const generateTableBody = (tableData)
+
 export default function ReactSimpleTable(props){
+
+  const [enablePagination, setEnablePagination] = useState(false);
 
   const testData = [{key1:"value1",key2:"value2"}, {key1:"value1",key2:"value2"}, {key1:"value1",key2:"value2"}]; //for testing
 
   const tableData = testData;
+  const rowsPerPage = 2;//for testing
+  const currPage = 1;
+  const numPages = (parseInt(tableData.length/rowsPerPage)) + ((tableData.length%rowsPerPage > 0)? 1: 0);
   const tableHeader = (<thead>
-                    <tr>
-                      {Object.keys(tableData[0]).map((value,index)=>(
-                        <th key={`theader${index}`}>
-                          {value}
-                        </th>))
-                      }
-                    </tr>
-                  </thead>);
+                        <tr>
+                          {Object.keys(tableData[0]).map((value,index)=>(
+                            <th key={`theader${index}`}>
+                              {value}
+                            </th>))
+                          }
+                        </tr>
+                      </thead>);
+  const tableFooter = (<tfoot>
+                        <tr>
+                          <td colSpan={Object.keys(tableData).length}>
+                            <button>
+                              Previous
+                            </button>
+                            <span>
+                              {currPage} of {numPages}
+                            </span>
+                            <button>
+                              Next
+                            </button>
+                          </td>
+                        </tr>
+                      </tfoot>);
 
   const tableBody = (<tbody>
-                  {tableData.map((object, index) =>(
-                    <tr key={`trow${index}`}>
-                      {Object.values(object).map((value, index) =>(
-                        <td key={`tdata${index}`}>
-                          {value}
-                        </td>))
+                      {
+
                       }
-                    </tr>))
-                  }
-                </tbody>);
+                    </tbody>);
 
 //  console.log(testData[0]);
     return(
       <table>
         {tableHeader}
         {tableBody}
+        {tableFooter}
       </table>
     );
 
