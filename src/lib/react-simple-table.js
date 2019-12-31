4cc27aafaebdfb9fb320243fd2c12b4008638264
Sreevishnu <sreeab28@gmail.com>
2019-12-31 14:14:36 +0530

    pagination implemented, layout fix needed

--- src/lib/react-simple-table.js
+++ src/lib/react-simple-table.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
+import './react-simple-table.css';
 import PropTypes from 'prop-types';
 
 const generateTableBody = (tableData)=>{
   const tableBody = (tableData.map((object, index)=>(
     <tr key={`trow${index}`}>
       {Object.values(object).map((value, index)=>(
-        <td key={`tdata${index}`}>
+        <td className={(isNaN(parseInt(value)))?"rst-align-lt rst-data":"rst-align-rt rst-data"} key={`tdata${index}`}>
           {value}
         </td>
       ))}
@@ -15,7 +16,7 @@ const generateTableBody = (tableData)=>{
 }
 
 const generateTableBodyPagination = (tableData, currPage,rowsPerPage)=>{
-  const tableDataPaginated = new Array();
+  const tableDataPaginated = [];
   
   for(var i = 0;i<tableData.length;i+=rowsPerPage)
     tableDataPaginated.push(tableData.slice(i,i+rowsPerPage));
@@ -25,11 +26,14 @@ const generateTableBodyPagination = (tableData, currPage,rowsPerPage)=>{
 
 export default function ReactSimpleTable(props){
 
-  const testData = [{key1:"value1",key2:"value2"}, {key1:"value1",key2:"value2"}, {key1:"value1",key2:"value2"}]; //for testing
+//  const testData = [{key1:"value1",key2:"key2"}, {key1:"value1",key2:"key2"}, {key1:"value1",key2:"key2"}]; //for testing
 
   const [currPage, setCurrPage] = useState(0);
-  const tableData = testData;//for testing, actual: props.tableData
-  const rowsPerPage = 2;//for testing, actual: props.rowsPerPage
+  const tableData = props.tableData;//for testing, actual: props.tableData
+  if(tableData.length % props.rowsPerPage > 0){
+    
+  }
+  const rowsPerPage = props.rowsPerPage;//for testing, actual: props.rowsPerPage
   const numPages = (parseInt(tableData.length / rowsPerPage)) + ((tableData.length % rowsPerPage > 0)? 1: 0);
 
   const handleNext = ()=>{
@@ -41,13 +45,13 @@ export default function ReactSimpleTable(props){
   }
 
   const disableNext = ()=>{
-    return (currPage == numPages-1)?true:false;
+    return (currPage === numPages-1)?true:false;
   }
   
   const disablePrevious = ()=>{
-    return (currPage == 0)?true:false;
+    return (currPage === 0)?true:false;
   }
-  const tableHeader = (<thead>
+  const tableHeader = (<thead >
                         <tr>
                           {Object.keys(tableData[0]).map((value,index)=>(
                             <th key={`theader${index}`}>
@@ -59,31 +63,46 @@ export default function ReactSimpleTable(props){
 
   const tableFooter = (<tfoot>
                         <tr>
-                          <td colSpan={Object.keys(tableData).length}>
-                            <button disabled={disablePrevious()} onClick={handlePrevious}>
-                              {'<'}
-                            </button>
-                            <span>
-                              {currPage+1} of {numPages}
+                          <td colSpan={`${Object.keys(tableData[0]).length}`}>
+                            <span className="rst-footer">
+                              <button className="ripple rst-btn" disabled={disablePrevious()} onClick={handlePrevious}>
+                                {'<'}
+                              </button>
+                              <span>
+                                {currPage+1} of {numPages}
+                              </span>
+                              <button className="ripple rst-btn" disabled={disableNext()} onClick={handleNext}>
+                                {'>'}
+                              </button>
                             </span>
-                            <button disabled={disableNext()} onClick={handleNext}>
-                              {'>'}
-                            </button>
                           </td>
                         </tr>
                       </tfoot>);
 
-  const tableBody = (<tbody>
-                      {(false)?generateTableBodyPagination(tableData,currPage,2):generateTableBody(tableData)/* for testing, actual:props.pagination */}
+  const tableBody = (<tbody >
+                      {(props.pagination)?generateTableBodyPagination(tableData,currPage,props.rowsPerPage):generateTableBody(tableData)/* for testing, actual:props.pagination */}
                     </tbody>);
 
 //  console.log(testData[0]);
     return(
-      <table>
-        {tableHeader}
-        {tableBody}
-        {/*props.pagination && */tableFooter }
-      </table>
+      <div className="rst-container">
+        <table className="rst-base">
+          {tableHeader}
+          {tableBody}
+          {props.pagination && tableFooter }
+        </table>
+      </div>
     );
 
+}
+
+ReactSimpleTable.propTypes = {
+  tableData: PropTypes.array.isRequired,
+  pagination: PropTypes.bool,
+  rowsPerPage : PropTypes.number,
+};
+
+ReactSimpleTable.defaultProps = {
+  pagination: false,
+  rowsPerPage: 2,
 }
--- src/module/index.js
+++ src/module/index.js
@@ -1,4 +1,120 @@
 import React from 'react';
 import ReactSimpleTable from '../lib';
-const App = ()=><ReactSimpleTable/>;
+
+const demoData = [
+	{
+		color: "red",
+		value: "#f00",
+    asd: "qwe"
+	},
+	{
+		color: "green",
+		value: "#0f0",
+    asd: "qwe"
+	},
+	{
+		color: "blue",
+		value: "#00f",
+    asd: "qwe"
+	},
+	{
+		color: "cyan",
+		value: "#0ff",
+    asd: "qwe"
+	},
+	{
+		color: "magenta",
+		value: "#f0f",
+    asd: "qwe"
+	},
+	{
+		color: "yellow",
+		value: "#ff0",
+    asd: "qwe"
+	},
+	{
+		color: "black",
+		value: "#000",
+    asd: "qwe"
+  },
+  {
+		color: "red",
+		value: "#f00",
+    asd: "qwe"
+	},
+	{
+		color: "green",
+		value: "#0f0",
+    asd: "qwe"
+	},
+	{
+		color: "blue",
+    value: "#00f",
+    asd: "qwe"
+	},
+	{
+		color: "cyan",
+		value: "#0ff",
+    asd: "qwe"
+	},
+	{
+		color: "magenta",
+		value: "#f0f",
+    asd: "qwe"
+	},
+	{
+		color: "yellow",
+		value: "#ff0",
+    asd: "qwe"
+	},
+	{
+		color: "black",
+		value: "#000",
+    asd: "qwe"
+  },
+  {
+		color: "red",
+		value: "#f00",
+    asd: "qwe"
+	},
+	{
+		color: "green",
+		value: "#0f0",
+    asd: "qwe"
+	},
+	{
+		color: "blue",
+		value: "#00f",
+    asd: "qwe"
+	},
+	{
+		color: "cyan",
+		value: "#0ff",
+    asd: "qwe"
+	},
+	{
+		color: "magenta",
+		value: "#f0f",
+    asd: "qwe"
+	},
+	{
+		color: "yellow",
+		value: "#ff0",
+    asd: "qwe"
+	},
+	{
+		color: "black",
+    value: "#000",
+    asd: "qwe"
+	},
+];
+
+const App = ()=>{return (
+  <React.Fragment>
+    <div>React Simple Table</div>
+    <ReactSimpleTable tableData={demoData} />
+    <div>With Pagination</div>
+    <ReactSimpleTable tableData={demoData} pagination rowsPerPage={5} />
+  </React.Fragment>
+);}
 export default App;
