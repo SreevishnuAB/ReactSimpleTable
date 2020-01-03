542d8e2b52b447bd3a8f305491208742f8ef9eb7
Sreevishnu <sreeab28@gmail.com>
2020-01-03 22:33:15 +0530

    fixing indentation

--- src/module/index.js
+++ src/module/index.js
@@ -5,116 +5,139 @@ const demoData = [
 	{
 		color: "red",
 		value: "#f00",
-    asd: "123"
+    asd: "123",
+    qwe: "qwe"
 	},
 	{
 		color: "green",
 		value: "#0f0",
-    asd: "23"
+    asd: "23",
+    qwe: "qwe"
 	},
 	{
 		color: "blue",
 		value: "#00f",
-    asd: "345"
+    asd: "345",
+    qwe: "qwe"
 	},
 	{
 		color: "cyan",
 		value: "#0ff",
-    asd: "6786"
+    asd: "6786",
+    qwe: "qwe"
 	},
 	{
 		color: "magenta",
 		value: "#f0f",
-    asd: "235"
+    asd: "235",
+    qwe: "qwe"
 	},
 	{
 		color: "yellow",
 		value: "#ff0",
-    asd: "353"
+    asd: "353",
+    qwe: "qwe"
 	},
 	{
 		color: "black",
 		value: "#000",
-    asd: "75678"
+    asd: "75678",
+    qwe: "qwe"
   },
   {
 		color: "red",
 		value: "#f00",
-    asd: "908"
+    asd: "908",
+    qwe: "qwe"
 	},
 	{
 		color: "green",
 		value: "#0f0",
-    asd: "2342"
+    asd: "2342",
+    qwe: "qwe"
 	},
 	{
 		color: "blue",
     value: "#00f",
-    asd: "122"
+    asd: "122",
+    qwe: "qwe"
 	},
 	{
 		color: "cyan",
 		value: "#0ff",
-    asd: "087"
+    asd: "087",
+    qwe: "qwe"
 	},
 	{
 		color: "magenta",
 		value: "#f0f",
-    asd: "4634"
+    asd: "4634",
+    qwe: "qwe"
 	},
 	{
 		color: "yellow",
 		value: "#ff0",
-    asd: "15414"
+    asd: "15414",
+    qwe: "qwe"
 	},
 	{
 		color: "black",
 		value: "#000",
-    asd: "786"
+    asd: "786",
+    qwe: "qwe"
   },
   {
 		color: "red",
 		value: "#f00",
-    asd: "2342"
+    asd: "2342",
+    qwe: "qwe"
 	},
 	{
 		color: "green",
 		value: "#0f0",
-    asd: "12321"
+    asd: "12321",
+    qwe: "qwe"
 	},
 	{
 		color: "blue",
 		value: "#00f",
-    asd: "56456"
+    asd: "56456",
+    qwe: "qwe"
 	},
 	{
 		color: "cyan",
 		value: "#0ff",
-    asd: "756"
+    asd: "756",
+    qwe: "qwe"
 	},
 	{
 		color: "magenta",
 		value: "#f0f",
-    asd: "6746"
+    asd: "6746",
+    qwe: "qwe"
 	},
 	{
 		color: "yellow",
 		value: "#ff0",
-    asd: "345"
+    asd: "345",
+    qwe: "qwe"
 	},
 	{
 		color: "black",
     value: "#000",
-    asd: "756"
+    asd: "756",
+    qwe: "qwe"
 	},
 ];
 
-const App = ()=>{return (
-  <React.Fragment>
-    <div>React Simple Table</div>
-    <ReactSimpleTable tableData={demoData} />
-    <div>With Pagination</div>
-    <ReactSimpleTable tableData={demoData} pagination rowsPerPage={5} />
-  </React.Fragment>
-);}
+const App = ()=>{
+  return (
+    <React.Fragment>
+      <div>React Simple Table</div>
+      <ReactSimpleTable tableData={demoData} />
+      <div>With Pagination</div>
+      <ReactSimpleTable tableData={demoData} pagination rowsPerPage={10} />
+    </React.Fragment>
+  );
+}
 export default App;
